a5f1b5d5ed7761e67b8e3a16758440146d80bf57
james <james.go@cshs.org>
2019-10-25 12:19:17 -0700

    remove old build packager script

--- electron/build-packager.ps1
+++
@@ -1,3 +0,0 @@
-.\node_modules\.bin\electron-packager --overwrite . pine
-Copy-Item "..\dist\changes_to_pine_final.exe" -Destination ".\pine-win32-x64\resources\pine_2.exe"
-#robocopy ..\dist\pine_2 .\pine-win32-x64\resources\pine_2 /E
