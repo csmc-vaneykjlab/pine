30d62d6970754d19c77ced1763e0d5eb469ba670
james <james.go@cshs.org>
2020-01-23 17:10:15 -0800

    updated links

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup (
   name="pine",
-  version="0.1.0",
+  version="0.1.1",
   description="Protein Interaction Network Extractor",
   license="Apache License 2.0",
   author_email="GroupHeartBioinformaticsSupport@cshs.org",
