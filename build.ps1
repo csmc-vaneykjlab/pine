6c68a67899128b772ccc802a532daf356adbeafc
Go <James.Go@cshs.org>
2020-04-28 23:27:53 -0700

    remove matplotlib warning

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 set "PATH=%PATH%;C:\Windows\System32\downlevel;"
 env\Scripts\activate.ps1
-pyinstaller --onedir --clean --noconfirm --hidden-import igraph.vendor.texttable .\pine\pine.py
+pyinstaller --onedir --clean --noconfirm --hidden-import igraph.vendor.texttable --exclude-module matplotlib .\pine\pine.py
 deactivate
